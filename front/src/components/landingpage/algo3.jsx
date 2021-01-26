8b7901d9a6fd9345336bc6985a6ecc98db6de22a
Jerónimo Santa María Illanes <jerosantamariai@MacBook-Pro-de-Jeronimo.local>
2021-01-26 16:17:25 -0300

    possible algo3

---
+++ front/src/components/landingpage/algo3.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const AlgoTres = props => {
+    return (
+        <div className="container3 d-flex">
+            <h1>¿Por qué elegir ProtegePyme?</h1>
+            <ul>
+                <li>Partner estratégico de CobranzaOnline, líder en la cobranza extrajudicial de facturas.</li>
+                <li>Trabajamos 100% online, facilitando la comunicación con nuestros clientes</li>
+                <li>Nos enfocamos 100% en las MiPymes</li>
+                <li>Somos un equipo legal experto en cobranzas.</li>
+                <li>Reportamos periódicamente del estado de los juicios para que puedas supervisarlos.</li>
+                <li>Trabajamos omnicanal, para responder cualquier consulta en menos de 24 hrs.</li>
+                <li>Te incorporamos a nuestro software. Cada vez que exista un movimiento, se te notificará a tu correo.</li>
+                <li>Transparentamos nuestros precios, para que no te lleves ninguna sorpresa.</li>
+            </ul>
+        </div>
+    );
+}
+
+export default AlgoTres;
